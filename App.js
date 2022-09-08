ca7c6964b61940d137888b31639c461360c84015
Lynh <chinsugaming@gmail.com>
2022-09-09 00:04:31 +0700

    bai 4

--- App.js
+++ App.js
@@ -3,15 +3,14 @@ import { Button, StatusBar, StyleSheet, Text, View, Alert } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <View style={styles.box} >
-        <Text>Hello, World!</Text>
-      </View> */}
-      <View>
-        <Button
-          title="button"
-          color="skyblue"
-          onPress={() => Alert.alert("hello")}
-        />
+      <View style={styles.box} >
+        <Text>Square 1</Text>
+      </View>
+      <View style={styles.box} >
+        <Text>Square 2</Text>
+      </View>
+      <View style={styles.box} >
+        <Text>Square 3</Text>
       </View>
     </View>
   );
@@ -20,16 +19,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: "space-evenly",
   },
-
   box: {
     backgroundColor: "skyblue",
-    width: 200,
-    height: 200,
+    width: 100,
+    height: 100,
+    margin: 10,
     alignItems: "center",
     justifyContent: "center",
   },
+  box1:{
+    backgroundColor: 'red'
+  },
+  box2:{
+    backgroundColor: 'blue'
+  },
+  box3:{
+    backgroundColor: 'yellow'
+  }
 });
